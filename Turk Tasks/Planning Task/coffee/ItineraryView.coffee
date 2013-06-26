f0e06ba0b905be8847574007a2c5f1837770b461
Joey Rafidi <jfidi731@gmail.com>
2013-06-25 21:18:12 -0700

    itinerary calendar work

--- Turk Tasks/Planning Task/coffee/ItineraryView.coffee
+++ Turk Tasks/Planning Task/coffee/ItineraryView.coffee
@@ -4,9 +4,6 @@ do ->
 
     initialize: =>
       @session = @options.session
-      @currentTaskModel = @session.currentTaskModel
-      @itineraryModel = @session.itineraryModel
-      @travelTimeModel = @session.travelTimeModel
 
     render: =>
       @$el.empty()
@@ -14,3 +11,148 @@ do ->
       source = $('#itinerary-view-template').html()
       template = Handlebars.compile(source)
       @$el.html template()
+
+      @calendar = new com.uid.crowdcierge.CalendarView
+        currentTaskModel: @session.currentTaskModel
+        itineraryModel: @session.itineraryModel
+        travelTimeModel: @session.travelTimeModel
+        activitiesModel: @session.activitiesModel
+
+      @$el.append @calendar.$el
+      @calendar.render()
+
+  _SLOT_SIZE_MINUTES = 15
+  _BACKGROUND_COLOR = '#D3F1F2'
+  _CALENDAR_HEIGHT = 578
+  _CALENDAR_OPTIONS =
+    height: _CALENDAR_HEIGHT
+    allDayDefault:false
+    events: []
+    allDaySlot: false
+    slotMinutes: _SLOT_SIZE_MINUTES
+    eventColor: "white",
+    eventBorderColor: "gray",
+    eventTextColor: "#000000"
+
+  # Too many hacks below
+  class com.uid.crowdcierge.CalendarView extends Backbone.View
+    className: 'calendar-wrapper'
+
+    initialize: =>
+      @currentTaskModel = @options.currentTaskModel
+      @itineraryModel = @options.itineraryModel
+      @travelTimeModel = @options.travelTimeModel
+      @activitiesModel = @options.activitiesModel
+
+      @listenTo @itineraryModel, 'add', @_addEvent
+      @listenTo @itineraryModel, 'remove', @_removeEvent
+      @listenTo @itineraryModel, 'reset', @_resetEvents
+
+    render: =>
+      @$el.empty()
+
+      source = $('#calendar-view-template').html()
+      template = Handlebars.compile(source)
+      @$el.html template()
+      @$calendar = @$('#calendar')
+
+      @_getTripTimes()
+
+      switch @currentTaskModel.get 'taskType'
+        when 'replan', 'inProgress'
+          @_initializeMidTripCalendar()
+        else
+          @_initializeStandardCalendar()
+
+      @_resetEvents(@itineraryModel)
+
+    _getTripTimes: =>
+      if @currentTaskModel.get('endTime') > 1440
+        @shift = (@currentTaskModel.get('endTime') - 1440) / 60
+      else
+        @shift = 0
+
+      if @currentTaskModel.get('date')?
+        tripDate = @currentTaskModel.get('date')
+        y = Math.round(tripDate/10000);
+        m = Math.round((tripDate % 10000) / 100) - 1;
+        d = tripDate % 100;
+        @date = new Date(y, m, d)
+      else
+        @date = new Date()
+
+      startHours = @_hours @currentTaskModel.get('beginTime')
+      startMin = @currentTaskModel.get('beginTime') % 60
+      @date.setHours(startHours - @shift)
+      @date.setMinutes(startMin)
+
+    _initializeMidTripCalendar: =>
+      # TODO
+
+    _initializeStandardCalendar: =>
+      begin = @currentTaskModel.get('beginTime') # - _SLOT_SIZE_MINUTES
+      begin = begin/60 - @shift
+      end = @currentTaskModel.get('endTime')/60 - @shift
+
+      @$calendar.fullCalendar(_.defaults(
+        minTime: begin
+        maxTime: end
+        editable: !(@currentTaskModel.get('taskType') == 'preview')
+        eventDrop: @_eventDrop
+        eventResize: @_eventResize
+        eventClick: @_eventClick
+        #eventRender: @_renderEvent
+        , _CALENDAR_OPTIONS))
+
+      @_modifyCalendarView()
+
+    _modifyCalendarView: =>
+      @$calendar.fullCalendar('changeView', 'agendaDay')
+      
+      # @$('.fc-slot0 .fc-widget-content div').text('Start');
+      # source = $('#end-calendar-template').html()
+      # template = Handlebars.compile(source)
+      # $end = $(template())
+      # @$('.fc-agenda-slots tbody').append $end
+
+    _addEvent: (model) =>
+      startDate = new Date(@date)
+      if model.get('start') != 0
+        startDate.setHours @_hours(model.get('start'))
+        startDate.setMinutes model.get('start')%60
+
+      endDate = new Date(@date)
+      endDate.setHours @_hours(model.get('duration')) + startDate.getHours()
+      endDate.setMinutes model.get('duration')%60 + startDate.getMinutes()
+
+      evt = 
+        id: model.id
+        title: model.get('name')
+        start: startDate
+        end: endDate
+      @$calendar.fullCalendar('renderEvent', evt, true)
+
+    _removeEvent: (model) =>
+      @$calendar.fullCalendar('removeEvents', model.id)
+      model.set 'start', 0
+
+    _resetEvents: (collection) =>
+      @$calendar.fullCalendar('removeEvents')
+      for model in collection.models
+        @_addEvent(model)
+
+    _eventResize: (evt, days, minutes) =>
+      model = @itineraryModel.get(evt.id)
+      oldDuration = model.get 'duration'
+      model.set 'duration', oldDuration + minutes
+
+    _eventDrop: (evt, days, minutes) =>
+      model = @itineraryModel.get(evt.id)
+      oldStart = model.get 'start'
+      model.set 'start', oldStart + minutes
+
+    _eventClick: (evt) =>
+      @activitiesModel.set 'selected', @itineraryModel.get(evt.id)
+
+    _hours: (min) =>
+      return Math.floor(min/60)
--- Turk Tasks/Planning Task/js/ItineraryView.js
+++ Turk Tasks/Planning Task/js/ItineraryView.js
@@ -5,9 +5,9 @@
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
   (function() {
-    var _ref;
+    var _BACKGROUND_COLOR, _CALENDAR_HEIGHT, _CALENDAR_OPTIONS, _SLOT_SIZE_MINUTES, _ref, _ref1;
 
-    return com.uid.crowdcierge.ItineraryView = (function(_super) {
+    com.uid.crowdcierge.ItineraryView = (function(_super) {
       __extends(ItineraryView, _super);
 
       function ItineraryView() {
@@ -19,10 +19,7 @@
       ItineraryView.prototype.className = 'itinerary-view';
 
       ItineraryView.prototype.initialize = function() {
-        this.session = this.options.session;
-        this.currentTaskModel = this.session.currentTaskModel;
-        this.itineraryModel = this.session.itineraryModel;
-        return this.travelTimeModel = this.session.travelTimeModel;
+        return this.session = this.options.session;
       };
 
       ItineraryView.prototype.render = function() {
@@ -31,10 +28,194 @@
         this.$el.empty();
         source = $('#itinerary-view-template').html();
         template = Handlebars.compile(source);
-        return this.$el.html(template());
+        this.$el.html(template());
+        this.calendar = new com.uid.crowdcierge.CalendarView({
+          currentTaskModel: this.session.currentTaskModel,
+          itineraryModel: this.session.itineraryModel,
+          travelTimeModel: this.session.travelTimeModel,
+          activitiesModel: this.session.activitiesModel
+        });
+        this.$el.append(this.calendar.$el);
+        return this.calendar.render();
       };
 
       return ItineraryView;
+
+    })(Backbone.View);
+    _SLOT_SIZE_MINUTES = 15;
+    _BACKGROUND_COLOR = '#D3F1F2';
+    _CALENDAR_HEIGHT = 578;
+    _CALENDAR_OPTIONS = {
+      height: _CALENDAR_HEIGHT,
+      allDayDefault: false,
+      events: [],
+      allDaySlot: false,
+      slotMinutes: _SLOT_SIZE_MINUTES,
+      eventColor: "white",
+      eventBorderColor: "gray",
+      eventTextColor: "#000000"
+    };
+    return com.uid.crowdcierge.CalendarView = (function(_super) {
+      __extends(CalendarView, _super);
+
+      function CalendarView() {
+        this._hours = __bind(this._hours, this);
+        this._eventClick = __bind(this._eventClick, this);
+        this._eventDrop = __bind(this._eventDrop, this);
+        this._eventResize = __bind(this._eventResize, this);
+        this._resetEvents = __bind(this._resetEvents, this);
+        this._removeEvent = __bind(this._removeEvent, this);
+        this._addEvent = __bind(this._addEvent, this);
+        this._modifyCalendarView = __bind(this._modifyCalendarView, this);
+        this._initializeStandardCalendar = __bind(this._initializeStandardCalendar, this);
+        this._initializeMidTripCalendar = __bind(this._initializeMidTripCalendar, this);
+        this._getTripTimes = __bind(this._getTripTimes, this);
+        this.render = __bind(this.render, this);
+        this.initialize = __bind(this.initialize, this);        _ref1 = CalendarView.__super__.constructor.apply(this, arguments);
+        return _ref1;
+      }
+
+      CalendarView.prototype.className = 'calendar-wrapper';
+
+      CalendarView.prototype.initialize = function() {
+        this.currentTaskModel = this.options.currentTaskModel;
+        this.itineraryModel = this.options.itineraryModel;
+        this.travelTimeModel = this.options.travelTimeModel;
+        this.activitiesModel = this.options.activitiesModel;
+        this.listenTo(this.itineraryModel, 'add', this._addEvent);
+        this.listenTo(this.itineraryModel, 'remove', this._removeEvent);
+        return this.listenTo(this.itineraryModel, 'reset', this._resetEvents);
+      };
+
+      CalendarView.prototype.render = function() {
+        var source, template;
+
+        this.$el.empty();
+        source = $('#calendar-view-template').html();
+        template = Handlebars.compile(source);
+        this.$el.html(template());
+        this.$calendar = this.$('#calendar');
+        this._getTripTimes();
+        switch (this.currentTaskModel.get('taskType')) {
+          case 'replan':
+          case 'inProgress':
+            this._initializeMidTripCalendar();
+            break;
+          default:
+            this._initializeStandardCalendar();
+        }
+        return this._resetEvents(this.itineraryModel);
+      };
+
+      CalendarView.prototype._getTripTimes = function() {
+        var d, m, startHours, startMin, tripDate, y;
+
+        if (this.currentTaskModel.get('endTime') > 1440) {
+          this.shift = (this.currentTaskModel.get('endTime') - 1440) / 60;
+        } else {
+          this.shift = 0;
+        }
+        if (this.currentTaskModel.get('date') != null) {
+          tripDate = this.currentTaskModel.get('date');
+          y = Math.round(tripDate / 10000);
+          m = Math.round((tripDate % 10000) / 100) - 1;
+          d = tripDate % 100;
+          this.date = new Date(y, m, d);
+        } else {
+          this.date = new Date();
+        }
+        startHours = this._hours(this.currentTaskModel.get('beginTime'));
+        startMin = this.currentTaskModel.get('beginTime') % 60;
+        this.date.setHours(startHours - this.shift);
+        return this.date.setMinutes(startMin);
+      };
+
+      CalendarView.prototype._initializeMidTripCalendar = function() {};
+
+      CalendarView.prototype._initializeStandardCalendar = function() {
+        var begin, end;
+
+        begin = this.currentTaskModel.get('beginTime');
+        begin = begin / 60 - this.shift;
+        end = this.currentTaskModel.get('endTime') / 60 - this.shift;
+        this.$calendar.fullCalendar(_.defaults({
+          minTime: begin,
+          maxTime: end,
+          editable: !(this.currentTaskModel.get('taskType') === 'preview'),
+          eventDrop: this._eventDrop,
+          eventResize: this._eventResize,
+          eventClick: this._eventClick
+        }, _CALENDAR_OPTIONS));
+        return this._modifyCalendarView();
+      };
+
+      CalendarView.prototype._modifyCalendarView = function() {
+        return this.$calendar.fullCalendar('changeView', 'agendaDay');
+      };
+
+      CalendarView.prototype._addEvent = function(model) {
+        var endDate, evt, startDate;
+
+        startDate = new Date(this.date);
+        if (model.get('start') !== 0) {
+          startDate.setHours(this._hours(model.get('start')));
+          startDate.setMinutes(model.get('start') % 60);
+        }
+        endDate = new Date(this.date);
+        endDate.setHours(this._hours(model.get('duration')) + startDate.getHours());
+        endDate.setMinutes(model.get('duration') % 60 + startDate.getMinutes());
+        evt = {
+          id: model.id,
+          title: model.get('name'),
+          start: startDate,
+          end: endDate
+        };
+        return this.$calendar.fullCalendar('renderEvent', evt, true);
+      };
+
+      CalendarView.prototype._removeEvent = function(model) {
+        this.$calendar.fullCalendar('removeEvents', model.id);
+        return model.set('start', 0);
+      };
+
+      CalendarView.prototype._resetEvents = function(collection) {
+        var model, _i, _len, _ref2, _results;
+
+        this.$calendar.fullCalendar('removeEvents');
+        _ref2 = collection.models;
+        _results = [];
+        for (_i = 0, _len = _ref2.length; _i < _len; _i++) {
+          model = _ref2[_i];
+          _results.push(this._addEvent(model));
+        }
+        return _results;
+      };
+
+      CalendarView.prototype._eventResize = function(evt, days, minutes) {
+        var model, oldDuration;
+
+        model = this.itineraryModel.get(evt.id);
+        oldDuration = model.get('duration');
+        return model.set('duration', oldDuration + minutes);
+      };
+
+      CalendarView.prototype._eventDrop = function(evt, days, minutes) {
+        var model, oldStart;
+
+        model = this.itineraryModel.get(evt.id);
+        oldStart = model.get('start');
+        return model.set('start', oldStart + minutes);
+      };
+
+      CalendarView.prototype._eventClick = function(evt) {
+        return this.activitiesModel.set('selected', this.itineraryModel.get(evt.id));
+      };
+
+      CalendarView.prototype._hours = function(min) {
+        return Math.floor(min / 60);
+      };
+
+      return CalendarView;
 
     })(Backbone.View);
   })();
