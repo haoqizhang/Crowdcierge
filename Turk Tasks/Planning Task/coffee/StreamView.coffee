e4efe7b6f7bf5bd012146ff85927f599863bafa8
Joey Rafidi <jfidi731@gmail.com>
2013-06-28 00:00:26 -0700

    stream updates

--- Turk Tasks/Planning Task/coffee/StreamView.coffee
+++ Turk Tasks/Planning Task/coffee/StreamView.coffee
@@ -2,6 +2,9 @@ do ->
   class com.uid.crowdcierge.StreamView extends Backbone.View
     className: 'stream-view'
 
+    events:
+      'keyup input': '_handleInputKeypress'
+
     initialize: =>
       @session = @options.session
       @itineraryModel = @session.itineraryModel
@@ -12,8 +15,6 @@ do ->
 
       @filterModel = new Backbone.Model
         keyword: ''
-
-      @listenTo @filterModel, 'change', @render
 
     render: =>
       @$el.empty()
@@ -43,6 +44,33 @@ do ->
       @$('.stream-item-list').append @todoItems.$el
       @$('.stream-item-list').append @activityItems.$el
 
+      @_initializeSearchbox()
+
+    _initializeSearchbox: =>
+      source = []
+      for model in @activitiesModel.get('items').models
+        source.push
+          label: model.get('name')
+          value: model.get('name')
+
+      for cat in @currentTaskModel.get('categories')
+        source.push
+          label: '#' + cat
+          value: '#' + cat
+
+      @$('input').autocomplete
+        source: source
+        minLength: 3
+
+    _handleInputKeypress: (evt) =>
+      if evt.keyCode == 13 or evt.which == 13
+        @filterModel.set 'keyword', @$('input').val()
+        @$('input').autocomplete('close')
+
+      if @$('input').val() == ''
+        @filterModel.set 'keyword', ''
+        @$('input').autocomplete('close')
+
   class TodoItemsView extends Backbone.View
     tag: 'tbody'
     id: 'sysStreamBody'
@@ -52,13 +80,17 @@ do ->
 
     initialize: =>
       @todoItemModel = @options.todoItemModel
+      @filterModel = @options.filterModel
 
       @listenTo @todoItemModel, 'add change remove reset', @render
+      @listenTo @filterModel, 'change', @render
       
     render: =>
       @$el.empty()
 
-      for model in @todoItemModel.models
+      filteredModels = _filterModels(@todoItemModel.models,
+        @filterModel.get('keyword'))
+      for model in filteredModels
         source = $("#todo-template").html();
         template = Handlebars.compile(source);
         $item = $(template(model));
@@ -84,19 +116,21 @@ do ->
     initialize: =>
       @activitiesModel = @options.activitiesModel
       @itineraryModel = @options.itineraryModel
+      @filterModel = @options.filterModel
 
       @listenTo @activitiesModel, 'add change remove reset', @render
       @listenTo @itineraryModel, 'add change remove reset', @render
+      @listenTo @filterModel, 'change', @render
 
     render: =>
       @$el.empty()
 
-      for model in @activitiesModel.get('items').models
+      filteredModels = _filterModels(@activitiesModel.get('items').models,
+        @filterModel.get('keyword'))
+      for model in filteredModels
         source = $("#stream-item-template").html();
         template = Handlebars.compile(source);
-
         index = _.indexOf(@itineraryModel.models, model) + 1
-
         $item = $(template(_.defaults {ind: index, inIt: index > 0}, 
           model));
         @$el.append $item
@@ -104,3 +138,17 @@ do ->
     _selectActivity: (evt) =>
       id = evt.currentTarget.id
       @activitiesModel.set 'selected', @activitiesModel.get('items').get(id)
+
+  _filterModels = (models, keyword) =>
+    result = []
+    for model in models
+      if model.get('name').indexOf(keyword) != -1 or model.get('description').indexOf(keyword) != -1
+        result.push(model)
+        continue
+
+      for cat in model.get 'categories'
+        if ('#' + cat) == keyword
+          result.push(model)
+          continue
+
+    return result
--- Turk Tasks/Planning Task/js/StreamView.js
+++ Turk Tasks/Planning Task/js/StreamView.js
@@ -5,18 +5,25 @@
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
   (function() {
-    var ActivityItemsView, CheckItemsView, TodoItemsView, _ref, _ref1, _ref2, _ref3;
+    var ActivityItemsView, CheckItemsView, TodoItemsView, _filterModels, _ref, _ref1, _ref2, _ref3,
+      _this = this;
 
     com.uid.crowdcierge.StreamView = (function(_super) {
       __extends(StreamView, _super);
 
       function StreamView() {
+        this._handleInputKeypress = __bind(this._handleInputKeypress, this);
+        this._initializeSearchbox = __bind(this._initializeSearchbox, this);
         this.render = __bind(this.render, this);
         this.initialize = __bind(this.initialize, this);        _ref = StreamView.__super__.constructor.apply(this, arguments);
         return _ref;
       }
 
       StreamView.prototype.className = 'stream-view';
+
+      StreamView.prototype.events = {
+        'keyup input': '_handleInputKeypress'
+      };
 
       StreamView.prototype.initialize = function() {
         this.session = this.options.session;
@@ -25,10 +32,9 @@
         this.todoItemModel = this.session.todoItemModel;
         this.checkItemModel = this.session.checkItemModel;
         this.currentTaskModel = this.session.currentTaskModel;
-        this.filterModel = new Backbone.Model({
+        return this.filterModel = new Backbone.Model({
           keyword: ''
         });
-        return this.listenTo(this.filterModel, 'change', this.render);
       };
 
       StreamView.prototype.render = function() {
@@ -56,7 +62,45 @@
         this.activityItems.render();
         this.$('.stream-item-list').append(this.checkItems.$el);
         this.$('.stream-item-list').append(this.todoItems.$el);
-        return this.$('.stream-item-list').append(this.activityItems.$el);
+        this.$('.stream-item-list').append(this.activityItems.$el);
+        return this._initializeSearchbox();
+      };
+
+      StreamView.prototype._initializeSearchbox = function() {
+        var cat, model, source, _i, _j, _len, _len1, _ref1, _ref2;
+
+        source = [];
+        _ref1 = this.activitiesModel.get('items').models;
+        for (_i = 0, _len = _ref1.length; _i < _len; _i++) {
+          model = _ref1[_i];
+          source.push({
+            label: model.get('name'),
+            value: model.get('name')
+          });
+        }
+        _ref2 = this.currentTaskModel.get('categories');
+        for (_j = 0, _len1 = _ref2.length; _j < _len1; _j++) {
+          cat = _ref2[_j];
+          source.push({
+            label: '#' + cat,
+            value: '#' + cat
+          });
+        }
+        return this.$('input').autocomplete({
+          source: source,
+          minLength: 3
+        });
+      };
+
+      StreamView.prototype._handleInputKeypress = function(evt) {
+        if (evt.keyCode === 13 || evt.which === 13) {
+          this.filterModel.set('keyword', this.$('input').val());
+          this.$('input').autocomplete('close');
+        }
+        if (this.$('input').val() === '') {
+          this.filterModel.set('keyword', '');
+          return this.$('input').autocomplete('close');
+        }
       };
 
       return StreamView;
@@ -82,17 +126,19 @@
 
       TodoItemsView.prototype.initialize = function() {
         this.todoItemModel = this.options.todoItemModel;
-        return this.listenTo(this.todoItemModel, 'add change remove reset', this.render);
+        this.filterModel = this.options.filterModel;
+        this.listenTo(this.todoItemModel, 'add change remove reset', this.render);
+        return this.listenTo(this.filterModel, 'change', this.render);
       };
 
       TodoItemsView.prototype.render = function() {
-        var $item, model, source, template, _i, _len, _ref2, _results;
+        var $item, filteredModels, model, source, template, _i, _len, _results;
 
         this.$el.empty();
-        _ref2 = this.todoItemModel.models;
+        filteredModels = _filterModels(this.todoItemModel.models, this.filterModel.get('keyword'));
         _results = [];
-        for (_i = 0, _len = _ref2.length; _i < _len; _i++) {
-          model = _ref2[_i];
+        for (_i = 0, _len = filteredModels.length; _i < _len; _i++) {
+          model = filteredModels[_i];
           source = $("#todo-template").html();
           template = Handlebars.compile(source);
           $item = $(template(model));
@@ -128,7 +174,7 @@
       return CheckItemsView;
 
     })(Backbone.View);
-    return ActivityItemsView = (function(_super) {
+    ActivityItemsView = (function(_super) {
       __extends(ActivityItemsView, _super);
 
       function ActivityItemsView() {
@@ -149,18 +195,20 @@
       ActivityItemsView.prototype.initialize = function() {
         this.activitiesModel = this.options.activitiesModel;
         this.itineraryModel = this.options.itineraryModel;
+        this.filterModel = this.options.filterModel;
         this.listenTo(this.activitiesModel, 'add change remove reset', this.render);
-        return this.listenTo(this.itineraryModel, 'add change remove reset', this.render);
+        this.listenTo(this.itineraryModel, 'add change remove reset', this.render);
+        return this.listenTo(this.filterModel, 'change', this.render);
       };
 
       ActivityItemsView.prototype.render = function() {
-        var $item, index, model, source, template, _i, _len, _ref4, _results;
+        var $item, filteredModels, index, model, source, template, _i, _len, _results;
 
         this.$el.empty();
-        _ref4 = this.activitiesModel.get('items').models;
+        filteredModels = _filterModels(this.activitiesModel.get('items').models, this.filterModel.get('keyword'));
         _results = [];
-        for (_i = 0, _len = _ref4.length; _i < _len; _i++) {
-          model = _ref4[_i];
+        for (_i = 0, _len = filteredModels.length; _i < _len; _i++) {
+          model = filteredModels[_i];
           source = $("#stream-item-template").html();
           template = Handlebars.compile(source);
           index = _.indexOf(this.itineraryModel.models, model) + 1;
@@ -183,6 +231,27 @@
       return ActivityItemsView;
 
     })(Backbone.View);
+    return _filterModels = function(models, keyword) {
+      var cat, model, result, _i, _j, _len, _len1, _ref4;
+
+      result = [];
+      for (_i = 0, _len = models.length; _i < _len; _i++) {
+        model = models[_i];
+        if (model.get('name').indexOf(keyword) !== -1 || model.get('description').indexOf(keyword) !== -1) {
+          result.push(model);
+          continue;
+        }
+        _ref4 = model.get('categories');
+        for (_j = 0, _len1 = _ref4.length; _j < _len1; _j++) {
+          cat = _ref4[_j];
+          if (('#' + cat) === keyword) {
+            result.push(model);
+            continue;
+          }
+        }
+      }
+      return result;
+    };
   })();
 
 }).call(this);
